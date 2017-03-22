88b84b8c073168766cd0c7e131820b57280f44b8
Nattawut <nattawut@thinknet.co.th>
2017-03-22 14:00:56 +0700

    refactor

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import logo from '../../public/images/logo.svg'
 const App = ({ children }) => (
   <div className="App">
     <div className="App-header">
-      <h2>Welcome to ReactJSs</h2>
+      <h2>Welcome to ReactJS</h2>
       <img src={logo} className="App-logo" alt="logo" />
     </div>
     <p className="App-intro">
